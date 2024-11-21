1f4a08f3c3f40df3c66c8b96440f3ec025c043a3
user <terrydjony@gmail.com>
2024-11-21 17:01:21 +0700

    init

---
+++ main.ts
@@ -0,0 +1,19 @@
+import { webhookCallback } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
+// You might modify this to the correct way to import your `Bot` object.
+import { bot } from "./bot.ts";
+
+const handleUpdate = webhookCallback(bot, "std/http");
+
+Deno.serve(async (req) => {
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    if (url.pathname.slice(1) === Deno.env.get("BOT_TOKEN")) {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+  return new Response();
+});
